89b0600076a34ea72b164cb667175be246250e4a
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-03-26 06:52:56 +0200

    add gh to hover the documentation

--- init.lua
+++ init.lua
@@ -530,6 +530,7 @@ require('lazy').setup({
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map('gh', vim.lsp.buf.hover, 'Hover Documentation')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
