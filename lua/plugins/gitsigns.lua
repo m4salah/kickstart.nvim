a6e36a766b0b42ef0f40ff6e0fffa1dd538f25f6
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-03-29 06:56:22 +0200

    refactor gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,22 @@
+-- Here is a more advanced example where we pass configuration
+-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+--    require('gitsigns').setup({ ... })
+--
+-- See `:help gitsigns` to understand what the configuration keys do
+return { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup()
+    vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>')
+    vim.keymap.set('n', '<leader>gt', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+  end,
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+}
