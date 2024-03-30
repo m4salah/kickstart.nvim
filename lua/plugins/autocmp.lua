3991313054f444a105951b2cef972505b2853b02
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-03-30 03:54:57 +0200

    for somereason c y doesn't work so i make enter for accepting the completion

--- lua/plugins/autocmp.lua
+++ lua/plugins/autocmp.lua
@@ -65,7 +65,7 @@ return { -- Autocompletion
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm { select = true },
 
         -- Manually trigger a completion from nvim-cmp.
         --  Generally you don't need this, because nvim-cmp will display
@@ -98,6 +98,7 @@ return { -- Autocompletion
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
+        { name = 'buffer' },
       },
     }
   end,
