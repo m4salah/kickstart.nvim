5596c75594402e3e37fd5f08e312d79f6cd2a4cd
Mohamed A. Salah <msalah@kenzz.com>
2024-04-04 05:09:04 +0200

    use prettier to format javascript and typescript files

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -19,7 +19,12 @@ return { -- Autoformat
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      --
+      -- java/typescript prettier
+      javascript = { { 'prettierd', 'prettier' } },
+      typescript = { { 'prettierd', 'prettier' } },
+      javascriptreact = { { 'prettierd', 'prettier' } },
+      typescriptreact = { { 'prettierd', 'prettier' } },
     },
   },
 }
