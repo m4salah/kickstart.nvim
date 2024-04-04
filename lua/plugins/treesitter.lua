d5894b917b7fb620d0d4acfefd5ee20b14e64947
Mohamed A. Salah <msalah@kenzz.com>
2024-04-04 04:56:20 +0200

    jsdoc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'go', 'typescript', 'tsx', 'fish', 'css' },
+    ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'go', 'typescript', 'tsx', 'fish', 'css', 'jsdoc', 'json' },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
