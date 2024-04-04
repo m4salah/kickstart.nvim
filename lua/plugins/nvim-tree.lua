264b6b9d20f338d9b1a68abf6f41ace324508852
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-04-04 20:17:36 +0200

    update the nvim tree to focus on the location of the file that currently opened

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,7 +6,11 @@ return { -- nvim tree
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('nvim-tree').setup {}
+    require('nvim-tree').setup {
+      update_focused_file = {
+        enable = true,
+      },
+    }
 
     -- leader n to toggle nvim tree
     vim.keymap.set('n', '<C-n>', vim.cmd.NvimTreeToggle)
