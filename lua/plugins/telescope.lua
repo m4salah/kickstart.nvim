d9d4c61bd13ce0c5aff221ce150ae569d7f0a410
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-06-29 08:39:50 +0300

    removig file browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -91,10 +91,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[,] Find existing buffers' })
-    -- Alternatively, using lua API
-    vim.keymap.set('n', '<leader>fb', function()
-      require('telescope').extensions.file_browser.file_browser()
-    end, { desc = '[F]ile [B]rowser' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
