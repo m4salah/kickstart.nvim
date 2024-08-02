7c1dc80a2cdf7e68881a26312f68a886e74277e7
Mohamed A. Salah <msalah@kenzz.com>
2024-08-02 15:32:48 +0300

    unused keymap anymore

--- init.lua
+++ init.lua
@@ -95,8 +95,6 @@ vim.keymap.set('i', 'jj', '<Esc>')
 
 -- save/quit keys
 vim.keymap.set('n', '<leader>w', vim.cmd.w)
-vim.keymap.set('n', '<leader>wq', vim.cmd.wq)
-vim.keymap.set('n', '<leader>wqa', vim.cmd.wqa)
 
 -- L to the end of the line
 vim.keymap.set({ 'n', 'v' }, 'L', '$')
