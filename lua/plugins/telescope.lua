0940ccede52e27b7e67eb068945d40719814c869
Mohamed A. Salah <msalah@kenzz.com>
2024-08-02 17:05:57 +0300

    add worktree keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -92,6 +92,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[B] Find existing buffers' })
+    -- git worktree commands
+    vim.keymap.set('n', '<Leader>sr', "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
+    vim.keymap.set('n', '<Leader>sR', "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>")
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
