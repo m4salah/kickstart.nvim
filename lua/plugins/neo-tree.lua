7939e279c0b85c153a003dd06705fa37a8c9697a
Mohamed A. Salah <me@msalah.net>
2024-08-03 04:38:01 +0300

    make it silent

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,5 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -9,58 +8,7 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
-  keys = {
-    { '<C-n>', ':Neotree filesystem reveal float<CR>', { desc = 'NeoTree reveal' } },
-  },
   buffers = { follow_current_file = { enabled = true } },
-  opts = {
-    filesystem = {
-      hijack_netrw_behavior = 'open_current',
-    },
-    window = {
-      mappings = {
-        ['Y'] = function(state)
-          -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
-          -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
-          local node = state.tree:get_node()
-          local filepath = node:get_id()
-          local filename = node.name
-          local modify = vim.fn.fnamemodify
-
-          local results = {
-            filepath,
-            modify(filepath, ':.'),
-            modify(filepath, ':~'),
-            filename,
-            modify(filename, ':r'),
-            modify(filename, ':e'),
-          }
-
-          -- absolute path to clipboard
-          local i = vim.fn.inputlist {
-            'Choose to copy to clipboard:',
-            '1. Absolute path: ' .. results[1],
-            '2. Path relative to CWD: ' .. results[2],
-            '3. Path relative to HOME: ' .. results[3],
-            '4. Filename: ' .. results[4],
-            '5. Filename without extension: ' .. results[5],
-            '6. Extension of the filename: ' .. results[6],
-          }
-
-          if i > 0 then
-            local result = results[i]
-            if not result then
-              return print('Invalid choice: ' .. i)
-            end
-            vim.fn.setreg('"', result)
-            vim.notify('Copied: ' .. result)
-          end
-        end,
-      },
-    },
-  },
-
   init = function()
     -- the remote file handling part
     vim.api.nvim_create_autocmd('BufEnter', {
@@ -92,6 +40,64 @@ return {
         end
       end,
     })
-    -- keymaps
+  end,
+  config = function()
+    local opts = { noremap = true, silent = true }
+    local map = vim.api.nvim_set_keymap
+    map('n', '<C-n>', ':Neotree filesystem reveal float<CR>', opts)
+    require('neo-tree').setup {
+      popup_border_style = 'rounded',
+      enable_git_status = true,
+      filesystem = {
+        hijack_netrw_behavior = 'open_current',
+        filtered_items = {
+          hide_dotfiles = false,
+          always_show = {
+            '*.env',
+          },
+        },
+      },
+      window = {
+        mappings = {
+          ['Y'] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+              filepath,
+              modify(filepath, ':.'),
+              modify(filepath, ':~'),
+              filename,
+              modify(filename, ':r'),
+              modify(filename, ':e'),
+            }
+
+            -- absolute path to clipboard
+            local i = vim.fn.inputlist {
+              'Choose to copy to clipboard:',
+              '1. Absolute path: ' .. results[1],
+              '2. Path relative to CWD: ' .. results[2],
+              '3. Path relative to HOME: ' .. results[3],
+              '4. Filename: ' .. results[4],
+              '5. Filename without extension: ' .. results[5],
+              '6. Extension of the filename: ' .. results[6],
+            }
+
+            if i > 0 then
+              local result = results[i]
+              if not result then
+                return print('Invalid choice: ' .. i)
+              end
+              vim.fn.setreg('"', result)
+              vim.notify('Copied: ' .. result)
+            end
+          end,
+        },
+      },
+    }
   end,
 }
