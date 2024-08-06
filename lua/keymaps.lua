afb2d42622e0abc2d87fd9519b89c122734f44d0
Mohamed A. Salah <msalah@kenzz.com>
2024-08-06 17:42:55 +0300

    fix: prev, next diagnositc

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -47,8 +47,8 @@ vim.opt.hlsearch = true
 keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', opts)
 
 -- Diagnostic keymaps
-keymap.set('n', '[d', vim.diagnostic.get_prev, { desc = 'Go to previous [D]iagnostic message' })
-keymap.set('n', ']d', vim.diagnostic.get_next, { desc = 'Go to next [D]iagnostic message' })
+keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
