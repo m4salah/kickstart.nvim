2f24a1e354565fd015dbf4f68cb335becbd0718a
Mohamed A. Salah <msalah@kenzz.com>
2024-08-07 19:37:33 +0300

    feat: use tabs in bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,20 @@
 return {
   'akinsho/bufferline.nvim',
   dependencies = 'nvim-tree/nvim-web-devicons',
+
   config = function()
     vim.opt.termguicolors = true
-    require('bufferline').setup {}
+    require('bufferline').setup {
+      options = {
+        mode = 'tabs',
+        always_show_bufferline = true,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        color_icons = true,
+      },
+    }
+
+    vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
   end,
 }
