4cd5019ed87fbd23133f16255cad5a830c38e0a3
Mohamed A. Salah <msalah@kenzz.com>
2024-08-25 10:02:16 +0300

    solving comment tsx

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,25 @@
 return {
   -- "gc" to comment visual regions/lines
   -- "gcc" toggle comment line
-  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'numToStr/Comment.nvim',
+    dependencies = {
+      -- for solving tsx comments
+      {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        config = function()
+          require('ts_context_commentstring').setup {
+            enable_autocmd = false,
+          }
+        end,
+      },
+    },
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
