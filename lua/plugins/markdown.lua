dd78c9958cd44b5de1af4a95f1a5598a16c2a54e
Mohamed A. Salah <me@msalah.net>
2024-08-29 20:24:47 +0300

    set keymap to markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -6,5 +6,8 @@ return {
     build = function()
       vim.fn['mkdp#util#install']()
     end,
+    config = function()
+      vim.keymap.set('n', '<leader>mp', '<CMD>MarkdownPreviewToggle<CR>', { desc = '[M]arkdown [P]review' })
+    end,
   },
 }
