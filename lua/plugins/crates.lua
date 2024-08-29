08ed6afa3e4db85e35385abb623724d8ed3b299f
Mohamed A. Salah <me@msalah.net>
2024-08-29 19:50:43 +0300

    setup crates

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -1,7 +1,17 @@
 return {
   'saecki/crates.nvim',
   tag = 'stable',
+  ft = { 'toml' },
   config = function()
-    require('crates').setup()
+    require('crates').setup {
+      completion = {
+        cmp = {
+          enabled = true,
+        },
+      },
+    }
+    require('cmp').setup.buffer {
+      sources = { { name = 'crates' } },
+    }
   end,
 }
