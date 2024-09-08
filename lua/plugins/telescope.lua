0beee938d1856489677218046f466adb952e089d
Mohamed A. Salah <msalah@kenzz.com>
2024-09-08 10:40:48 +0300

    install new plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -94,6 +94,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[B] Find existing buffers' })
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = '[G]it [S]tatus' })
 
     -- git worktree commands
     vim.keymap.set('n', '<Leader>sr', "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
