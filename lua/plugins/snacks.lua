1dbbd6ff69af35d520bba0b486df0c8ee07e9f7a
Mohamed A. Salah <msalah@kenzz.com>
2024-12-06 18:30:54 +0200

    setup snacks pluging

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,9 +2,27 @@ return {
   'folke/snacks.nvim',
   opts = {
     lazygit = {
-      -- your lazygit configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      enabled = true,
+    },
+
+    git = {
+      enabled = true,
+    },
+
+    gitbrowse = {
+      enabled = true,
+    },
+    terminal = {
+      enabled = true,
+    },
+
+    scratch = {
+      enabled = true,
+    },
+    words = {
+      enabled = true,
+    },
+    bigfile = {
       enabled = true,
     },
   },
@@ -29,6 +47,67 @@ return {
         Snacks.lazygit.log()
       end,
       desc = 'Lazygit Log (cwd)',
+    },
+    {
+      '<leader>gB',
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = 'Git Browse',
+    },
+    {
+      '<leader>gb',
+      function()
+        Snacks.git.blame_line()
+      end,
+      desc = 'Git Blame Line',
+    },
+
+    {
+      '<c-/>',
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'Toggle Terminal',
+    },
+    {
+      '<c-_>',
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'which_key_ignore',
+    },
+
+    {
+      '<leader>S',
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = 'Select Scratch Buffer',
+    },
+
+    {
+      '<leader>.',
+      function()
+        Snacks.scratch()
+      end,
+      desc = 'Toggle Scratch Buffer',
+    },
+    {
+      ']]',
+      function()
+        Snacks.words.jump(vim.v.count1)
+      end,
+      desc = 'Next Reference',
+      mode = { 'n', 't' },
+    },
+    {
+      '[[',
+      function()
+        Snacks.words.jump(-vim.v.count1)
+      end,
+      desc = 'Prev Reference',
+      mode = { 'n', 't' },
     },
   },
 }
